054cc5355a7c9613cb72bb84bf3c639b84c7f375
Curtis <curtis@live.fr>
2022-03-01 12:42:18 +0100

    CGU et password

--- auth-data.js
+++ auth-data.js
@@ -5,81 +5,87 @@ const form = document.querySelector('#form')
 const togglePassword = document.querySelector('#togglePassword')
 
 togglePassword.onclick = () => {
-    togglePassword.classList.toggle("blue")
-    password.type === "password"
-       ? password.type = "text"
-       : password.type = "password";
- }
+   togglePassword.classList.toggle("blue")
+   password.type === "password"
+      ? password.type = "text"
+      : password.type = "password";
+}
 
 function regexText(x) {
-    const regexText = /^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'\s-]{1,40}$/;
-    return RegExp(regexText).test(x);
- }
+   const regexText = /^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'\s-]{1,40}$/;
+   return RegExp(regexText).test(x);
+}
 
- function regexTextNumbers(x) {
+function regexTextNumbers(x) {
    const regexTextNumbers = /^[0-9a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'\s-_]{1,40}$/;
    return RegExp(regexTextNumbers).test(x);
 }
 
- function regexEmail(x) {
-    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return RegExp(regexEmail).test(x.toLowerCase().trim());
- }
+function regexEmail(x) {
+   const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return RegExp(regexEmail).test(x.toLowerCase().trim());
+}
 
- function regexPassword(x) {
-    const regexStrongPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[.!@#$%^&*]).{8,20}$/;
-    return RegExp(regexStrongPassword).test(x);
- }
+function regexPassword(x) {
+   const regexStrongPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[.!@#$%^&*]).{8,20}$/;
+   return RegExp(regexStrongPassword).test(x);
+}
 
- function displayValidity(element, regex, message) {
-    (!regex(element.value))
-       ? setError(element, `Merci d'entrer un ${message} valide`)
-       : setSuccess(element);
- }
+function displayValidity(element, regex, message) {
+   (!regex(element.value))
+      ? setError(element, `Merci d'entrer un ${message} valide`)
+      : setSuccess(element);
+}
 
- function setError(element, message) {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+function displayPasswordValidity(element, regex) {
+   (!regex(element.value))
+      ? setError(element, `Le mot de passe doit contenir 8 caractères : 1 majuscule, 1 minscule, 1 chiffre, 1 caractère spécial.`)
+      : setSuccess(element);
+}
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('shake', 'error');
-    inputControl.classList.remove('success')
+function setError(element, message) {
+   const inputControl = element.parentElement;
+   const errorDisplay = inputControl.querySelector('.error');
 
-    setTimeout(() => {
-       inputControl.classList.remove('shake');
-    }, 500)
- }
+   errorDisplay.innerText = message;
+   inputControl.classList.add('shake', 'error');
+   inputControl.classList.remove('success')
 
- function setSuccess(element, message) {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+   setTimeout(() => {
+      inputControl.classList.remove('shake');
+   }, 500)
+}
 
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
+function setSuccess(element, message) {
+   const inputControl = element.parentElement;
+   const errorDisplay = inputControl.querySelector('.error');
 
- };
+   errorDisplay.innerText = '';
+   inputControl.classList.add('success');
+   inputControl.classList.remove('error');
 
- function validBox() {
-    const validatedBox = document.querySelector('.validated-box');
-    validatedBox.style.display = 'flex';
+};
 
-    setTimeout(() => {
-       validatedBox.style.display = 'none'
-    }, 1000)
- }
+function validBox() {
+   const validatedBox = document.querySelector('.validated-box');
+   validatedBox.style.display = 'flex';
 
- function redirection() {
+   setTimeout(() => {
+      validatedBox.style.display = 'none'
+   }, 1000)
+}
+
+function redirection() {
    setTimeout(() => {
       window.location.replace(`${window.location.origin}/P7_test/accueil.html?id=Sullivan`)
    }, 500)
- }
+}
 
- function getId() {
+function getId() {
    let params = new URLSearchParams(window.location.search);
    let userId = params.get("id")
    const welcomeMsg = document.querySelector('#welcome-msg');
-   
+
    setTimeout(() => {
       welcomeMsg.textContent = `Bienvenue ${userId}`;
    }, 1000)
